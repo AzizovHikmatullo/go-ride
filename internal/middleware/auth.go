849800096f910dd7b43d6384c4c9825525384e40
AzizovHikmatullo <azizov.hikmatullo07@gmail.com>
2026-01-20 23:16:13 +0500

    update middlewares

--- internal/middleware/auth.go
+++ internal/middleware/auth.go
@@ -1,46 +1,52 @@
 package middleware
 
 import (
-	"fmt"
 	"net/http"
 	"os"
 	"strings"
 
+	"github.com/AzizovHikmatullo/go-ride/internal/auth"
 	"github.com/gin-gonic/gin"
-	"github.com/golang-jwt/jwt"
+	"github.com/golang-jwt/jwt/v5"
 )
 
-func AuthMiddleware(requiredRoles ...string) gin.HandlerFunc {
+func AuthMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		header := c.GetHeader("Authorization")
-		if header == "" {
-			c.AbortWithStatusJSON(http.StatusUnauthorized, map[string]any{"error": "missing token"})
+		authHeader := c.GetHeader("Authorization")
+		if authHeader == "" {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Authorization header missing"})
 			return
 		}
 
-		tokenStr := strings.TrimPrefix(header, "Bearer ")
-		token, err := jwt.Parse(tokenStr, func(token *jwt.Token) (interface{}, error) {
-			return []byte(os.Getenv("JWT_SECRET")), nil
-		})
+		tokenStr := strings.TrimPrefix(authHeader, "Bearer ")
+		if tokenStr == authHeader {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Bearer token missing"})
+			return
+		}
 
+		token, err := jwt.ParseWithClaims(
+			tokenStr,
+			&auth.Claims{},
+			func(token *jwt.Token) (interface{}, error) {
+				if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+					return nil, jwt.ErrSignatureInvalid
+				}
+				return []byte(os.Getenv("JWT_SECRET")), nil
+			},
+		)
 		if err != nil || !token.Valid {
-			fmt.Println(token)
-			c.AbortWithStatusJSON(http.StatusUnauthorized, map[string]any{"error": "invalid token"})
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "invalid token"})
 			return
 		}
 
-		claims := token.Claims.(jwt.MapClaims)
-		role := claims["role"].(string)
-
-		for _, requiredRole := range requiredRoles {
-			if requiredRole != "" && role != requiredRole {
-				c.AbortWithStatusJSON(http.StatusUnauthorized, map[string]any{"error": "forbidden"})
-				return
-			}
+		claims, ok := token.Claims.(*auth.Claims)
+		if !ok {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "invalid claims"})
+			return
 		}
 
-		c.Set("userID", int(claims["userID"].(float64)))
-		c.Set("role", role)
+		c.Set("userID", claims.UserID)
+		c.Set("role", claims.Role)
 
 		c.Next()
 	}
